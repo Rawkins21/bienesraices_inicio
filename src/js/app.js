3f84707252205b5ae6cd89dc455a9d33dc974e9e
Rawkins21 <rawkins1111@gmail.com>
2022-05-23 16:15:17 -0300

    edit

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkmode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDarkmode.matches); imprime en consola si darkmode esta activado en el s.o
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
