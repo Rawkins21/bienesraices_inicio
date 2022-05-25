630d427157db192a5173eff4088b2838dabcedd3
Rawkins21 <rawkins1111@gmail.com>
2022-05-25 14:02:30 -0300

    edit

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,14 @@ function darkmode() {
         document.body.classList.remove('dark-mode');
     }
 
+    prefiereDarkMode.addEventListener('change', function() { //cambio automatico del darkmode al cambiar la config del s.o
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
