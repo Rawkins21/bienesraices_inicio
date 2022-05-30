a619d51133e072f27a00dffdf91b737045e15aa5
Rawkins21 <rawkins1111@gmail.com>
2022-05-30 15:55:51 -0300

    edit

---
+++ entrada.php
@@ -0,0 +1,42 @@
+<?php include 'includes/templates/header.php'; ?>
+
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Guía para la decoracion de tu hogar</h1>
+
+        <picture>
+            <source srcset="build/img/destacada2.webp" type="image/webp">
+            <source srcset="build/img/destacada2.jpg" type="image/jpeg">
+            <img src="build/img/destacada2.jpg" alt="imagen de la propiedad" loading="lazy">
+        </picture>
+        <p class="informacion-meta">Escrito el: <span>20/10/2022</span> por: <span>Admin</span></p>
+        <div class="resumen-propiedad">
+            <p>
+                Ahí va el Capitán Beto por el espacio Con su nave de fibra hecha en Haedo Ayer colectivero Hoy amo entre los amos del aire Ya lleva 15 años en su periplo Su equipo es tan precario como su destino Sin embargo, un anillo extraño Ahuyenta sus peligros en
+                el cosmos Ahí va el Capitán Beto por el espacio La foto de Carlitos sobre el comando Y un banderín de River Plate Y la triste estampita de un santo
+            </p>
+            <p>
+                ¿Dónde está el lugar al que todos llaman cielo? Si nadie viene hasta aquí a cebarme unos amargos, como en mi viejo umbral ¿Por qué habré venido hasta aquí, si no puedo más de soledad? Ya no puedo más de soledad Su anillo lo inmuniza en los peligros Pero
+                no lo protege de la tristeza Surcando la galaxia del Hombre Ahí va el Capitán Beto, el errante ¿Dónde habrá una ciudad en la que alguien silbe un tango? ¿Dónde están, dónde están los camiones de basura, mi vieja y el café? Si esto sigue
+                así como así, ni una triste sombra quedará Ni una triste sombra quedará Ni una triste sombra quedará
+            </p>
+            <p>Ahí va el Capitán Beto por el espacio Regando los malvones de su cabina Sin brújula y sin radio Jamás podrá volver a la Tierra Tardaron muchos años hasta encontrarlo El anillo de Beto llevaba inscripto un signo del alma</p>
+        </div>
+    </main>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.php">Nosotros</a>
+                <a href="anuncios.php">Anuncios</a>
+                <a href="blog.php">Blog</a>
+                <a href="contacto.php">Contacto</a>
+
+            </nav>
+        </div>
+        <p class="copyright">Todos los derechos Reservados 2022 &copy;</p>
+    </footer>
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+
+</html>
---
+++ nosotros.php
@@ -0,0 +1,71 @@
+<?php include 'includes/templates/header.php'; ?>
+
+    <main class="contenedor seccion">
+        <h1>Conoce sobre Nosotros</h1>
+
+        <div class="contenido-nosotros">
+            <div class="imagen">
+                <picture>
+                    <source srcset="build/img/nosotros.webp" type="image/webp">
+                    <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                    <img src="build/img/nosotros.jpg" alt="Sobre Nosotros" loading="lazy">
+                </picture>
+            </div>
+            <div class="texto-nosotros">
+                <blockquote>
+                    25 años de experiencia
+                </blockquote>
+                <p>
+                    Ahí va el Capitán Beto por el espacio Con su nave de fibra hecha en Haedo Ayer colectivero Hoy amo entre los amos del aire Ya lleva 15 años en su periplo Su equipo es tan precario como su destino Sin embargo, un anillo extraño Ahuyenta sus peligros en
+                    el cosmos Ahí va el Capitán Beto por el espacio La foto de Carlitos sobre el comando Y un banderín de River Plate Y la triste estampita de un santo
+                </p>
+                <p>
+                    ¿Dónde está el lugar al que todos llaman cielo? Si nadie viene hasta aquí a cebarme unos amargos, como en mi viejo umbral ¿Por qué habré venido hasta aquí, si no puedo más de soledad? Ya no puedo más de soledad Su anillo lo inmuniza en los peligros Pero
+                    no lo protege de la tristeza Surcando la galaxia del Hombre Ahí va el Capitán Beto, el errante ¿Dónde habrá una ciudad en la que alguien silbe un tango? ¿Dónde están, dónde están los camiones de basura, mi vieja y el café? Si esto
+                    sigue así como así, ni una triste sombra quedará Ni una triste sombra quedará Ni una triste sombra quedará
+                </p>
+                <p>Ahí va el Capitán Beto por el espacio Regando los malvones de su cabina Sin brújula y sin radio Jamás podrá volver a la Tierra Tardaron muchos años hasta encontrarlo El anillo de Beto llevaba inscripto un signo del alma</p>
+            </div>
+        </div>
+    </main>
+
+    <section class="contenedor seccion">
+        <h1>Más Sobre Nosotros</h1>
+
+        <div class="iconos-nosotros">
+            <div class="icono">
+                <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
+                <h3>Seguridad</h3>
+                <p>Yo que nací sin poder Yo que luché por la libertad Pero nunca la pude tener Yo que viví entre fachistas Yo que morí en el altar Yo que nací con los que estaban bien Pero a la noche estaba todo mal Hoy paso el tiempo (Demoliendo hoteles)
+                    Mientras los plomos juntan los cables Cazan rehenes</p>
+            </div>
+            <div class="icono">
+                <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+                <h3>Precio</h3>
+                <p>Hoy paso el tiempo (Demoliendo hoteles) Mientras los chicos allá en la esquina Pegan carteles Yo fui educado con odio Y odiaba la humanidad Un día me fui con los hippies y tuve un amor y mucho más</p>
+            </div>
+            <div class="icono">
+                <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+                <h3>Tiempo</h3>
+                <p>Ahora no estoy más tranquilo Y por qué tendría que estar Todos crecimos sin entender Y todavía me siento un anormal Hoy paso el tiempo (Demoliendo hoteles) Mientras los plomos juntan los cables Cazan rehenes</p>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.php">Nosotros</a>
+                <a href="anuncios.php">Anuncios</a>
+                <a href="blog.php">Blog</a>
+                <a href="contacto.php">Contacto</a>
+
+            </nav>
+        </div>
+        <p class="copyright">Todos los derechos Reservados 2022 &copy;</p>
+    </footer>
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+
+</html>
