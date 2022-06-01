68dfb0b01c84785290447fa42467fbfebf853b66
Rawkins21 <rawkins1111@gmail.com>
2022-06-01 15:45:50 -0300

    edit

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,12 @@
 <?php 
-require '../../includes/funciones.php'; // ../../ sale de las carpetas propiedades y admin para entrar entrar en includes
+require '../includes/funciones.php'; // ../../ sale de las carpetas propiedades y admin para entrar entrar en includes
 incluirTemplate('header'); 
 ?>
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
+
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
     </main>
 
     <?php 
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,6 +5,8 @@ incluirTemplate('header');
 
     <main class="contenedor seccion">
         <h1>Crear</h1>
+
+        <a href="/admin" class="boton boton-verde">Volver</a>
     </main>
 
     <?php 
