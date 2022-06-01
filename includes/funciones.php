f1c86334f1970ddc8023ad7e737074084e93f6ed
Rawkins21 <rawkins1111@gmail.com>
2022-06-01 15:24:42 -0300

    edit

--- includes/funciones.php
+++ includes/funciones.php
@@ -2,7 +2,7 @@
 
 require 'app.php';
 
-function incluirTemplate( $nombre, $inicio = false ){ // $inicio = false se asigna por defecto si la clase inicio no esta presente, agrega la clase inicio a la pagina si esta presente
+function incluirTemplate( string $nombre, bool $inicio = false ){ // $inicio = false se asigna por defecto si la clase inicio no esta presente, agrega la clase inicio a la pagina si esta presente
     
     include TEMPLATES_URL."/${nombre}.php";
 }
