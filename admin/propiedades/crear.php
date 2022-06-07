0948c910a26e4f42f12fdc1becd7f88574e84978
Rawkins21 <rawkins1111@gmail.com>
2022-06-07 15:57:01 -0300

    edit

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,32 @@ incluirTemplate('header');
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form action="formulario">
+            <fieldset>
+                <legend>información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Ttiulo de Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio de Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png"> <!--//accept especifica los formatos a aceptar -->
+
+                <label for="descripcion">Descripción</label>
+                <textarea id="descripcion" cols="30" rows="10"></textarea>
+
+            </fieldset>
+        </form>
+
+        <fieldset>
+            <legend>informacion de Propiedad</legend>
+            <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3">
+
+        </fieldset>
     </main>
 
     <?php 
