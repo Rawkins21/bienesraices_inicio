2007223f0dfc1ecf6ffbcc6d1154742b52ab5181
Rawkins21 <rawkins1111@gmail.com>
2022-07-13 22:25:44 -0300

    edit

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB(){
     $db = mysqli_connect('localhost', 'root', '1991', 'bienes_raices');
-
+    $db->set_charset('utf8'); // fuerza el estandar utf-8 para solucionar errores de escritura como el uso de ñ 
     //  if($db) { 
     //      echo "se conecto";
     //  } else{
