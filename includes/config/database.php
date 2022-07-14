4e5f48d4d9230d3eedbd991ba09bff7e9f178c9f
Rawkins21 <rawkins1111@gmail.com>
2022-07-13 21:00:13 -0300

    edit

--- includes/config/database.php
+++ includes/config/database.php
@@ -3,10 +3,9 @@
 function conectarDB(){
     $db = mysqli_connect('localhost', 'root', '1991', 'bienes_raices');
 
-     if($db) { 
-         echo "se conecto";
-     } else{
-         echo "no se conecto";
-     }
-    //  } comprobar si se conecto a la base de datos
+    //  if($db) { 
+    //      echo "se conecto";
+    //  } else{
+    //      echo "no se conecto";
+    //   } comprobar si se conecto a la base de datos
 }
